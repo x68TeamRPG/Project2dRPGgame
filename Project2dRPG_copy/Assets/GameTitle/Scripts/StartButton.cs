c6988872fae85a32ff0bc1d471755424df93909e
mocococococo <tomomichi.kanaeda@paiza.co.jp>
2024-07-03 21:08:16 +0900

    コピープロジェクトの作成

---
+++ Project2dRPG_copy/Assets/GameTitle/Scripts/StartButton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+
+public class StartButton : MonoBehaviour
+{
+    private void Start()
+    {
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(() => 
+            {
+                SceneManager.LoadScene("FieldScene");
+            }
+        );
+    }
+
+}
