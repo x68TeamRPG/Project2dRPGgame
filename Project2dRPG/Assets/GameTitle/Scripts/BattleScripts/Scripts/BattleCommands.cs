91f3f8ffdd507d5c1a462ee32147e988ed484eb2
mocococococo <tomomichi.kanaeda@paiza.co.jp>
2025-06-19 20:16:21 +0900

    エフェクト挿入タイミングのコメント追加

--- Project2dRPG/Assets/GameTitle/Scripts/BattleScripts/Scripts/BattleCommands.cs
+++ Project2dRPG/Assets/GameTitle/Scripts/BattleScripts/Scripts/BattleCommands.cs
@@ -16,26 +16,29 @@ public class BattleCommands : MonoBehaviour
         yield return new WaitForSeconds(1.0f);
         Waza normalAttack = new Waza();
         int damage = normalAttack.Execute(attacker, blocker);
-        yield return StartCoroutine(DamageRoll(damage, blocker));
         yield return StartCoroutine(textController.Write($"{attacker.name}の{normalAttack.jpname}！"));
+        // エフェクト
         yield return StartCoroutine(textController.Write($"{blocker.name}に{damage}のダメージ"));
+        yield return StartCoroutine(DamageRoll(damage, blocker));
     }
 
     public IEnumerator UseItem(Status Target, Item item)
     {
         yield return new WaitForSeconds(1.0f);
-        itemController.UseItem(item.name);
         yield return StartCoroutine(textController.Write($"{Target.name}は{item.jpname}を使用した！"));
-        yield return StartCoroutine(textController.Write($"{Target.name}のHPが{item.healAmount}回復した！"));
+        // エフェクト
+        yield return StartCoroutine(textController.Write($"{Target.name}のHPが{item.healAmount}回復した！"));        itemController.UseItem(item.name);
+        itemController.UseItem(item.name);
     }
 
     public IEnumerator UseWaza(Status attacker, Status blocker, Waza waza)
     {
         yield return new WaitForSeconds(1.0f);
         int damage = waza.Execute(attacker, blocker);
-        yield return StartCoroutine(DamageRoll(damage, blocker));
         yield return StartCoroutine(textController.Write($"{attacker.name}の{waza.jpname}！"));
+        // エフェクト
         yield return StartCoroutine(textController.Write($"{blocker.name}に{damage}ダメージ！"));
+        yield return StartCoroutine(DamageRoll(damage, blocker));
     }
 
     protected IEnumerator DamageRoll(int damage, Status blocker)
